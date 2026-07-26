08500e7ee7c838fa7c35d6c127804a3f896bfda2
erickjcss <151551835+erickjcss@users.noreply.github.com>
2026-07-26 14:22:40 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -289,9 +289,7 @@ useEffect(() => {
    <div className='  h-[80vh]'>
          <section className='populares ml-auto mr-auto 2 min-[700px]:w-[48vw] min-[700px]:mx-0 '>
         <div className='min-[700px]:w-[80vw] min-[700px]:mx-auto'>  
-                <h1 className='max-w-[220px] min-[700px]:max-w-[300px] text-base min-[700px]:text-2xl min-[1024px]:text-2xl bg-slate-100 relative rounded-md p-2'>
-                    Perfumes
-                </h1>
+               <h1 className=' max-w-[220px]      min-[700px]:max-w-[300px]    mb-2 text-base min-[700px]:text-2xl mx-auto min-[1024px]:text-2xl bg-slate-100 relative '>Perfumes</h1>
         </div>
       <div>
         <Perfumes setPerfumes={setPerfumes} setPerfumesAuxAux={setPerfumesAuxAux} perfumes={perfumes} perfumesAuxAux={perfumesAuxAux} />  
